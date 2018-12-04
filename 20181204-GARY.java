2535639288ce08b2cb3e24b35481ddd4507621f5
Usernameisyangqilin <45375840+Usernameisyangqilin@users.noreply.github.com>
2018-12-04 09:12:42 +0800

    Create 20181204-GARY.java

---
+++ 20181204-GARY.java
@@ -0,0 +1,25 @@
+public class Hiddenword{
+
+public String HW；
+public String Guess；
+
+public Hiddenword(String HW){
+}
+
+public String getHint(String Guess){
+
+for(i = 0; i<HW.length;i++){
+if(HW.getIndexOf(i)equals(Guess.getIndexOf(i))
+HW.getIndexOf(i).equalTo(Guess.getIndexOf(i));
+
+else if(HW.getIndexOf(i).equals(!Guess.getIndexOf(i))
+HW.getIndexOf(i).equalTo("+");
+
+else
+HW.getIndexOf(i).equalTo("*");
+
+}
+
+}
+
+}
