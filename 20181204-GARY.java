af2bc8873ecc28f11a271e79aac6c80f8c16cdce
Usernameisyangqilin <45375840+Usernameisyangqilin@users.noreply.github.com>
2018-12-04 09:19:05 +0800

    Update 20181204-GARY.java

--- 20181204-GARY.java
+++ 20181204-GARY.java
@@ -1,8 +1,7 @@
 public class Hiddenword{
 
-public String HW；
-
-public String Guess；
+private String HW；
+private String Guess；
 
 public Hiddenword(String HW){
 }
@@ -10,10 +9,10 @@ public Hiddenword(String HW){
 public String getHint(String Guess){
 
 for(i = 0; i<HW.length;i++){
-if(HW.getIndexOf(i)equals(Guess.getIndexOf(i))
+if(HW.getIndexOf(i)equalsTo(Guess.getIndexOf(i))
 HW.getIndexOf(i).equalTo(Guess.getIndexOf(i));
 
-else if(HW.getIndexOf(i).equals(!Guess.getIndexOf(i))
+else if(HW.getIndexOf(i).equalTo(!Guess.getIndexOf(i))
 HW.getIndexOf(i).equalTo("+");
 
 else
